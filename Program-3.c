f892c0c5d2bf6f2ed7983f6988bb51f88a1fea17
chinnu-1237 <84765368+chinnu-1237@users.noreply.github.com>
2021-08-24 20:46:09 +0530

    Update Program-3.c

--- Program-3.c
+++ Program-3.c
@@ -1,17 +1,35 @@
+
 //Input: 10 20 
+
 //Output: 30
+
 #include<stdio.h>
+
 struct sum
+
 {
+
 int a;
+
 int b;
+
 };
+
 int main()
+
 {
+
 int sum1;
+
 struct sum s;
+
 printf("Enter two numbers:");
+
 scanf("%d%d",&s.a,&s.b);
-//compute sum
+
+sum1=s.a+s.b;
+
 printf("Sum=%d",sum1);
+
 }
+

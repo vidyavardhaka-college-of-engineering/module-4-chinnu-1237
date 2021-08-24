b8b600e2cba38bfe68a20fc547fc52926a518ad5
chinnu-1237 <84765368+chinnu-1237@users.noreply.github.com>
2021-08-24 21:21:09 +0530

    Update Program-8.c

--- Program-8.c
+++ Program-8.c
@@ -1,22 +1,38 @@
 //To compute the sum of all elements in an array using pointers
 
+ 
+
 //Input: 5 2 3 4 5 6 
-//Output:Sum=30 
+
+//Output:Sum=30
+
+ 
 
 #include <stdio.h>
-Int main() 
+
+int main() 
+
 {
-   int arr1[10];
-   int i,n, sum = 0;
-   int *pt;
-   printf(" Input the number of elements to store in the array (max 10) and enter the elements: ");
-   scanf("%d",&n);
-   for(i=0;i<n;i++)
-   {
-    scanf("%d",&arr1[i]);
-    }
-   
-pt = arr1; // pt store the base address of array arr1
+
+   int arr1[10];
+
+   int i,n, sum = 0;
+
+   int *pt;
+
+   printf(" Input the number of elements to store in the array (max 10) and enter the elements: ");
+
+   scanf("%d",&n);
+
+   for(i=0;i<n;i++)
+
+      {
+
+         scanf("%d",&arr1[i]);
+
+         }           
+
+  pt = arr1; // pt store the base address of array arr1
 
   for (i = 0; i < n; i++)
 
@@ -33,4 +49,6 @@ pt = arr1; // pt store the base address of array arr1
   return 0;
 
 }
+
+
 

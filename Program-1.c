380edc43f6ee0c1fcb7f39cbca404ceca2544ce9
chinnu-1237 <84765368+chinnu-1237@users.noreply.github.com>
2021-08-25 10:40:33 +0530

    Update Program-1.c

--- Program-1.c
+++ Program-1.c
@@ -1,33 +1,26 @@
-//Input : 23  34 
-
+//Input : 23  34 
 //Output: Sum of feet = 57
 
- 
-
 //Program to add two distances (feet-inch)
-
 #include <stdio.h>
 struct Distance
-
 {
-int feet;
-dist1, dist2, sum;
-}
- int main()
+    int feet;
+} dist1, dist2, sum;
 
+int main()
 {
-
-    printf("Enter feet  value for 1st and 2nd distance: ");
+    printf("Enter feet  value for 1st and 2nd distance: ");
     scanf("%d%d", &dist1.feet, &dist2.feet);
+    // adding feet
+    sum.feet = dist1.feet + dist2.feet;
+   
 
-    // adding feet
-
-    sum.feet = dist1.feet + dist2.feet;
     printf("Sum of feet = %d",sum.feet);
-
-    return 0;
-
+    return 0;
 }
+
+
 
 
 

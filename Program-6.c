5dc78256b03f68d503ec05d8839969275134aab0
chinnu-1237 <84765368+chinnu-1237@users.noreply.github.com>
2021-08-25 11:09:03 +0530

    Update Program-6.c

--- Program-6.c
+++ Program-6.c
@@ -1,24 +1,17 @@
-
 //Program in C to add two numbers using pointers.
-
-//Input:5 6                                                                                   
-
+//Input:5 6                                                                                   
 // Output: Sum=11
-
 #include <stdio.h>
-
 int main()
-
 {
-
-   int fno, sno, *ptr, *qtr, sum;
+   int fno, sno, *ptr, *qtr, sum;
    printf(" Input two numbers: ");
    scanf("%d%d", &fno,&sno);
    ptr = &fno;
    qtr = &sno;
    sum=*ptr+*qtr;
    printf(" Sum=%d",sum);
-   return 0;
-
+    return 0;
 }
+
 
